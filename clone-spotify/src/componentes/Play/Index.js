2416e8d796c9cc9f9e6e1729de38277e637af7b6
Fernando Filho <fernando.fs.filhorn@gmail.com>
2023-04-16 21:35:26 -0300

    timing da musica
    O timing da musica está completo em questão de código, basta estilizar

--- clone-spotify/src/componentes/Play/Index.js
+++ clone-spotify/src/componentes/Play/Index.js
@@ -12,57 +12,49 @@ import SegundoFormatado  from './Tempo/SegundoFormatado.js'
 
 const Play = (props) => {
 
-    const TempoCompletoDaMusica = 120
+    const TempoCompletoDaMusica = 100
 
     const [porcentagem, setPorcentagem] = useState(0)
-    const [tempoDaMusicaPercorrido, setTempoDaMusicaPercorrido] = useState(`00:00`)
-    // const [tempoDaMusicaPercorrido, setTempoDaMusicaPercorrido] = useState(QuantoTempoDaMusicaPassou(TempoCompletoDaMusica, porcentagem))
+    const [tempoDaMusicaPercorrido, setTempoDaMusicaPercorrido] = useState(QuantoTempoDaMusicaPassou())
     const [tempoDaMusicaQueFalta, setTempoDaMusicaQueFalta] = useState(TempoDaMusicaFormatado(TempoCompletoDaMusica))
 
     function TempoDaMusicaFormatado(tempo){
 
-        return(
-                `${MinutoFormatado(tempo)}:${SegundoFormatado(tempo)}` 
-            )
+        return( `${MinutoFormatado(tempo)}:${SegundoFormatado(tempo)}` )
     }
 
-    function QuantoTempoDaMusicaPassou(TempoCompletoDaMusica, PorcentagemDaMusica){
+    function QuantoTempoDaMusicaPassou(){
 
-        const TempoPercorrido = Math.trunc((TempoCompletoDaMusica*PorcentagemDaMusica)/100)
+        const TempoPercorrido = Math.trunc((TempoCompletoDaMusica*porcentagem)/100)
 
+        return( TempoDaMusicaFormatado(TempoPercorrido) )
+    }
+
+    function QuantoTempoDaMusicaFalta(){
+
+        const TempoQueFalta = Math.trunc((TempoCompletoDaMusica*(100-porcentagem))/100)
+
+        return( TempoDaMusicaFormatado(TempoQueFalta) )
+    }
+
+    function RecalculoDoTimingDaMusica(props){
         return(
-            console.log(`${MinutoFormatado(TempoPercorrido)}:${SegundoFormatado(TempoPercorrido)}`),
-
-            `${MinutoFormatado(TempoPercorrido)}:${SegundoFormatado(TempoPercorrido)}`
+            setPorcentagem(props.target.value),
+            setTempoDaMusicaPercorrido(QuantoTempoDaMusicaPassou()),
+            setTempoDaMusicaQueFalta(QuantoTempoDaMusicaFalta())
         )
     }
 
-    function QuantoTempoDaMusicaFalta(TempoCompletoDaMusica, PorcentagemDaMusica){
+    // function teste () {
+    //     return(
+    //         setPorcentagem(porcentagem + 1),
+    //         console.log(porcentagem)
+    //     )
+    // }
 
-        const TempoQueFalta = Math.trunc((TempoCompletoDaMusica*(100-PorcentagemDaMusica))/100)
-
-        return(
-            console.log(`${MinutoFormatado(TempoQueFalta)}:${SegundoFormatado(TempoQueFalta)}`),
-
-            `${MinutoFormatado(TempoQueFalta)}:${SegundoFormatado(TempoQueFalta)}`
-        )
-    }
-
-    function RecalculoDoTimingDaMusica(TempoCompletoDaMusica, PorcentagemDaMusica){
-        return(
-            console.log('deu certo'),
-            setTempoDaMusicaPercorrido(() => QuantoTempoDaMusicaPassou(TempoCompletoDaMusica, porcentagem)),
-            setTempoDaMusicaQueFalta(() => QuantoTempoDaMusicaFalta(TempoCompletoDaMusica, porcentagem))
-        )
-    }
-
-
-    function teste (){
-        return(
-            setPorcentagem(porcentagem + 1),
-            console.log(porcentagem)
-        )
-    }
+    // function pararTeste () {
+    //         clearInterval
+    // }
 
     return (
         <div className="Play">
@@ -72,13 +64,13 @@ const Play = (props) => {
                 <button/><GiPreviousButton/> */}
 
                 <button 
-                    onClick={setInterval(teste, 1000)}
+                    // onClick={setInterval(teste, 1000)}
                 >
                     <GiPlayButton/>
                 </button>
 
                 <button 
-                    // onClick={setTimeout(teste, 1000)}
+                    // onClick={setTimeout(pararTeste, 1000)}
                 >
                     <GiPauseButton/>
                 </button>
@@ -88,40 +80,28 @@ const Play = (props) => {
             </div>
 
             <div className='TimeBar'>
-                <TempoDaMusicaPercorrido Timing={tempoDaMusicaPercorrido}/>
+                <TextoDoTempoDaMusica Timing={tempoDaMusicaPercorrido}/>
 
+                <input
+                    type="range"
+                    value={porcentagem}
+                    min={0}
+                    step={1}
+                    max={100}
+                    onChange={value => setPorcentagem(value.target.value)}
+                    // onChange={RecalculoDoTimingDaMusica}
+                    onMouseMoveCapture={RecalculoDoTimingDaMusica}
+                    ></input>
 
-                    <input
-                        type="range"
-                        value={porcentagem}
-                        min={0}
-                        step={1}
-                        max={100}
-                        onChange={value => setPorcentagem(value.target.value)}
-                        // onMouseMoveCapture={() => setTempoDaMusicaPercorrido(() => QuantoTempoDaMusicaPassou(TempoCompletoDaMusica, porcentagem))}
-                        onMouseMoveCapture={() => RecalculoDoTimingDaMusica(TempoCompletoDaMusica, porcentagem)}
-                        // oninput={UpDateTime()}
-                     ></input>
-
-                {/* <p>{TempoDaMusicaFormatado(TempoCompletoDaMusica)}</p> */}
-                <TempoDaMusicaPercorrido Timing={tempoDaMusicaQueFalta}/>
+                <TextoDoTempoDaMusica Timing={tempoDaMusicaQueFalta}/>
             </div>
-
+            
         </div>
     )
 }
 
-const TempoDaMusicaPercorrido = ({Timing}) => {
-    return (
-        <div>{Timing}</div>
-    )
+const TextoDoTempoDaMusica = ({Timing}) => {
+    return ( <div>{Timing}</div> )
 }
-const TempoDaMusicaQueFalta = ({Timing}) => {
-    return (
-        <div>{Timing}</div>
-    )
-}
-
-
 
 export default Play
